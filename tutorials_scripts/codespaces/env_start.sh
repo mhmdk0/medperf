c3bb07c11314e73c19022217eb43837f394c522e
mhmdk0 <mohammadkassim54.mk@gmail.com>
2026-08-10 19:54:10 +0300

    update Docs and codespaces scripts - fix 3

--- tutorials_scripts/codespaces/env_start.sh
+++ tutorials_scripts/codespaces/env_start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Preparing local medperf server..."
 # we are located at /workspaces/medperf/ where repo is cloned to
-pip install -e ./cli
+pip install -e .
 pip install -r server/requirements.txt
 pip install -r server/test-requirements.txt
 medperf profile activate local
